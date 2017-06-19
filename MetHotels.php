f7b9a6410e7bdafab17f8c55154dfb8aefb1f237
natalijakrstic2360 <natalija.krstic.2360@metropolitan.ac.rs>
2017-06-19 18:37:24 +0200

    Add files via upload

---
+++ MetHotels.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="en">
+	<head>
+	    <meta charset="utf-8">
+	    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css">
+	    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js"></script>
+	</head>
+	<body>
+	    <div class="container">
+	    	<div class="row">
+	    		<h3>IT255-DZ01-Natalija_Krstic_2360</h3>
+	    	</div>
+			<div class="row">
+				<table class="table table-striped table-bordered">
+			        <thead>
+			            <tr>
+			                <th>Natalija</th>
+			                <th>Krstic</th>
+			                <th>2360</th>
+			                <th>SI</th>
+			                <th>FIT</th>
+			            </tr>
+			        </thead>
+		        </table>
+	    	</div>
+	    </div>
+	</body>
+</html>
